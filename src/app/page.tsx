8ced95df898ff1338b8bc22e271cf41e867e8fa1
Peter Kerfoot <peterckerfoot@gmail.com>
2025-09-09 11:08:33 -0500

    created public facing homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     // I want be able to include bg-home-img here after bg-black below
     <div className='bg-black bg-home-img min-h-screen'>
-      <main>
-        <div>
-          <h1>
+      <main className='flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh'>
+        <div className='flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl'>
+          <h1 className='text-4xl font-bold'>
             Dan&apos;s Computer
             <br /> Repair Shop
           </h1>
